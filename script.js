1f9555b5a6f0e3cbe14ca49a9c5d66ed09f3337b
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2025-09-25 18:28:55 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+function setup(){
+    createCanvas(800, 800);
+    imageMode(CENTER);
+    textAlign(CENTER, CENTER);
+}
+
+function draw(){
+    background(0);
+    push();
+
+
+
+    pop();
+}
