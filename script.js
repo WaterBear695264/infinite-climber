b335da5c659fb270f7bc17dd48b3df5d5e40b315
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2025-09-26 18:22:37 +0000

    changealanges

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const rockArray = [];
+class Rock {
+    constructor(pos, rotating, falling, trembling, actionspeed){
+        this.pos = pos
+        this.rotating = rotating;
+        this.falling = falling;
+        this.trembling = trembling;
+        this.actionspeed = actionspeed;
+    }
+
+    rotate(){
+
+    }
+
+    fall(){
+
+    }
+
+    tremble(){
+
+    }
+
+}
+
+rockTest = New Rock(New Vector(5, 10), false, false, false, 5);
+
+
+function draw(){
+    background(0);
+    push();
+    Array.foreach((element) => console.log(this.pos));
+
+
+    pop();
+}
+
+
+
 
 
 
@@ -5,13 +43,4 @@ function setup(){
     createCanvas(800, 800);
     imageMode(CENTER);
     textAlign(CENTER, CENTER);
-}
-
-function draw(){
-    background(0);
-    push();
-
-
-
-    pop();
 }
